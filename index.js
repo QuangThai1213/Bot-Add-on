92ed6590b19ee96bb516bb5a17855b0a601334cf
thaitq7141 <thaitq7141@co-well.com.vn>
2020-11-30 11:18:22 +0700

    change to googlesheet

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-// const WebSocket = require('ws');
-// const wss = new WebSocket.Server({ port: 8080 });
-// wss.on('connection', function connection(ws) {
-//     ws.on('message', function incoming(message) {
-//         jsonMessage = JSON.parse(message);
-//         console.log(message)
-//         console.log(jsonMessage)
-//         var clientResponse = {
-//             "status": 200,
-//         };
-//         clientResponse.message = [];
-//         // clientResponse.message.push(jsonMessage);
-//         ws.send(clientResponse);
-//     });
-// });
 const readXlsxFile = require('read-excel-file/node');
 const fs = require('fs');
 var json2xls = require('json2xls');
 const json = require('./asset/item.json')
 
-function getFirstID(item) {
-    let data = item.id.split(',')
-    if (Array.isArray(data) && data.length) {
-        return Number(data[0]);
-    }
-    return 0
-}
-let data = JSON.stringify(json.sort(function (a, b) {
-    return getFirstID(a) - getFirstID(b);
-}));
+// function getFirstID(item) {
+//     let data = item.id.split(',')
+//     if (Array.isArray(data) && data.length) {
+//         return Number(data[0]);
+//     }
+//     return 0
+// }
+// let data = JSON.stringify(json.sort(function (a, b) {
+//     return getFirstID(a) - getFirstID(b);
+// }));
 
-fs.writeFileSync('item.json', data);
+// fs.writeFileSync('item.json', data);
 
 // const schema = {
 //     'ID': {
@@ -44,6 +29,27 @@ fs.writeFileSync('item.json', data);
 //     console.log(rows)
 // })
 
-var xls = json2xls(json);
+// var xls = json2xls(json);
 
-fs.writeFileSync('data.xlsx', xls, 'binary');
+// fs.writeFileSync('data.xlsx', xls, 'binary');
+require('dotenv').config()
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+// spreadsheet key is the long id in the sheets URL
+const doc = new GoogleSpreadsheet('1RBHdyI-yAgvksAlKyQj-RKRACon1DoFVPeXN0Fz_lys');
+
+async function getData() {
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
+    await doc.loadInfo(); // loads document properties and worksheets
+    console.log(doc.title);
+    const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id] or doc.sheetsByTitle[title]
+    await sheet.loadHeaderRow()
+    console.log(sheet.title);
+    console.log(sheet.resize);
+
+}
+
+getData();
